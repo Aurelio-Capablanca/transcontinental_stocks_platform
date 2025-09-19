e804b69e5a683370e3f4ea86641a119fe9c54a51
Aurelio <aurelio.iempereur@gmail.com>
2025-09-18 22:43:47 -0600

    initial tests for SQL and set mods

---
+++ src/adapters/database/db_pool.rs
@@ -0,0 +1,4 @@
+fn create_pool() -> Result<(), Box<dyn std::error::Error>>{
+    
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
+use tokio_postgres::{Error, NoTls};
+mod adapters;
 
 
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    
+
+    
+    let (client, connection) = tokio_postgres::connect("host=localhost user=superuserp password=jkl555 dbname=transcontinental_stocks ", NoTls)
+    .await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await{
+            eprint!("Connection error : {}", e);
+        }
+    });
+
+    let constellation = "Sagittarius".to_string();
+    let number_rows = client.execute("insert into test_data (strings) values ($1)", &[&constellation ]).await.unwrap();
+    println!("executed : {:?}",number_rows);
+    let rows = client.query("select * from test_data td", &[]).await.unwrap();
+    for r in rows {
+        let id : i32 = r.get(0);
+        let value : String = r.get(1);
+        println!("id={:?}, col={:?}",id,value);
+    }
+
+    Ok(())
 }
