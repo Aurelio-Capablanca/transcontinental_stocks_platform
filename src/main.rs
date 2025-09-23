ba24178e52207072ace30bc9b229af9b77c3af1e
Aurelio <aurelio.iempereur@gmail.com>
2025-09-22 21:29:17 -0600

    Removing PostgreSQL to MongoDB
    changing MongoDB for PostgreSQL

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,9 @@
-use tokio_postgres::{Error, NoTls};
 mod adapters;
 
 
-
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    
-    let (client, connection) = tokio_postgres::connect("host=localhost user=superuserp password=jkl555 dbname=transcontinental_stocks ", NoTls)
-    .await?;
-
-    tokio::spawn(async move {
-        if let Err(e) = connection.await{
-            eprint!("Connection error : {}", e);
-        }
-    });
-
-    let constellation = "Sagittarius".to_string();
-    let number_rows = client.execute("insert into test_data (strings) values ($1)", &[&constellation ]).await.unwrap();
-    println!("executed : {:?}",number_rows);
-    let rows = client.query("select * from test_data td", &[]).await.unwrap();
-    for r in rows {
-        let id : i32 = r.get(0);
-        let value : String = r.get(1);
-        println!("id={:?}, col={:?}",id,value);
-    }
 
     Ok(())
 }
