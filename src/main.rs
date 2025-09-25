21afb7fa83fe187dec4ad6aa22f25b6a3d78c13e
Aurelio <aurelio.iempereur@gmail.com>
2025-09-24 23:54:17 -0600

    adding mods

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
+mod adapters;
+mod business;
 
 use tokio_postgres::GenericClient;
-
 use crate::adapters::database::db_pool;
 use std::sync::Arc;
 
-mod adapters;
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
-    let manager = db_pool::create_postgres_pool().await?;
+    let manager = Arc::new(db_pool::create_postgres_pool().await?);
     let client = manager.client();
 
     let constellation = "Odin".to_string();
-    let number_rows = client.execute("insert into test_data (strings) values ($1)", &[&constellation ]).await.unwrap();
+    let number_rows = client.execute("insert into test_data (strings) values ($1)", &[&constellation]).await.unwrap();
     println!("executed : {:?}",number_rows);
     let rows = client.query("select * from test_data td", &[]).await.unwrap();
     
